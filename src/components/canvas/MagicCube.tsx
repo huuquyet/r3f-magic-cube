fec244d9c05ff496a56f7ca1cac76a301054c4b4
Quyet <huuquyetng@gmail.com>
2024-05-02 12:30:19 +0700

    chore: add short order of cube

--- src/components/canvas/MagicCube.tsx
+++ src/components/canvas/MagicCube.tsx
@@ -3,6 +3,7 @@
 import { Center, Text3D, useCursor } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useRef, useState } from 'react'
+import type { Group } from 'three'
 
 const NUMBER_LIST = {
   8: [-1, 1, 1],
@@ -35,7 +36,7 @@ const NUMBER_LIST = {
 }
 
 export const MagicCube = ({ ...props }) => {
-  const ref = useRef(null)
+  const ref = useRef<Group>(null!)
   useFrame((state, delta) => {
     const t = state.clock.getElapsedTime()
     ref.current.rotation.x = (Math.sin(t) * Math.PI) / 8
@@ -68,5 +69,27 @@ const NumberNode = ({ text, ...props }) => {
         <meshPhysicalMaterial transparent roughness={0.5} color={hovered ? 'hotpink' : 'orange'} />
       </Text3D>
     </Center>
+  )
+}
+
+export const MagicCubeShort = ({ ...props }) => {
+  const ref = useRef<Group>(null!)
+  const arrNumber = [
+    18, 4, 20, 2, 27, 13, 22, 11, 9, 5, 21, 16, 25, 14, 3, 12, 7, 23, 19, 17, 6, 15, 1, 26, 8, 24,
+    10,
+  ]
+  useFrame((state, delta) => {
+    const t = state.clock.getElapsedTime()
+    ref.current.rotation.x = (Math.sin(t) * Math.PI) / 8
+    ref.current.rotation.y += delta / 4
+    ref.current.rotation.z = delta / 4
+  })
+
+  return (
+    <group ref={ref} {...props}>
+      {arrNumber.map((e, i) => (
+        <NumberNode key={i} text={e} position={[i % 3, Math.floor(i / 9), Math.floor(i / 3) % 3]} />
+      ))}
+    </group>
   )
 }
