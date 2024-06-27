aba63f3e6cf0af386faea3e9b0a85c9063221c87
Quyet <huuquyetng@gmail.com>
2024-06-27 17:02:37 +0700

    fix: workbox config commonjs

--- workbox-config.mjs
+++
@@ -1,8 +0,0 @@
-const config = {
-  globDirectory: 'dist/',
-  globPatterns: ['**/*.{ico,json,md,svg}'],
-  swDest: 'dist/sw.js',
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-}
-
-export default config
